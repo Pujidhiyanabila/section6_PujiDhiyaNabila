66de209b788656be174da3de5232cc85e011afea
Pujidhiyanabila <nabilapuji26@gmail.com>
2022-03-02 15:55:27 +0700

    Section 6 Soal 1 Fix

--- praktikum/bin/soal1.dart
+++ praktikum/bin/soal1.dart
@@ -1,12 +1,12 @@
 void main (){
-  var nilai = 90;
-  if (nilai > 70){   //jika usia kurang dari 20, maka ditampilkan remaja pada layar.
+  var nilai = 85;
+  if (nilai > 70){   //jika nilai besar dari 70, maka ditampilkan nilai A pada layar.
     print("Nilai A");
-  } else if (nilai > 40){  //jika usia kurang dari 40, maka ditampilkan dewasa pada layar.
+  } else if (nilai > 40){  //jika nilai besar dari 40, maka ditampilkan nilai B pada layar.
     print("Nilai B");
-  } else if (nilai > 0){
+  } else if (nilai > 0){   //jika nilai besar dari 0, maka ditampilkan nilai C pada layar.
     print("Nilai C");
   } else {
-    print("");  //jika tidak, maka ditampilkan tidak memiliki golongan pada layar.
+    print("");  //jika tidak, maka ditampilkan text kosong pada layar.
   }
 }
